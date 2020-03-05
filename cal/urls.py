c0f23957c51f24c5c069502c8bfbdafec390f5fc
AASunasara <akbar.h.sunasara@gmail.com>
2020-03-05 15:55:04 +0530

    messages added

--- cal/urls.py
+++ cal/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path, include
 from . import views
-from django.contrib.auth import views as auth_views
-
-
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='login.html',  redirect_authenticated_user=True), name='login'),
+    path('', views.LoginView, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('home/', views.home, name='home'),
     path('day/', views.day, name='day'),
--- cal/views.py
+++ cal/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.contrib.auth import logout
+from django.contrib.auth import logout, update_session_auth_hash
 from django.contrib import messages
-from django.contrib.auth import update_session_auth_hash
 from django.views.generic.edit import CreateView
 from .models import days, items
 from .forms import ItemsForm, DaysForm
+from django.contrib.auth.forms import AuthenticationForm, authenticate
+from django.contrib.auth.views import  auth_login
+
+
+def LoginView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                auth_login(request, user)
+                return redirect('home')
+        else:
+            messages.error(request,'Username or Password may not correct!.', extra_tags='login')
+            return redirect('login')
+
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
 @login_required(login_url='login')
 def home(request):
@@ -26,24 +47,6 @@ def home(request):
     else:
         form = ItemsForm(request.POST or None)
         return render(request, 'home.html', {'form': form})
-
-@login_required(login_url='login')
-def edit_item(request, id=None):
-    instance = get_object_or_404(items, id=id )
-    form = ItemsForm(request.POST or None , instance = instance )
-    if form.is_valid():
-        instance = form.save(commit =  False)
-        instance.save()
-    return render(request, 'home.html', {'form':form})
-
-@login_required(login_url='login')
-def edit_day(request, id=None):
-    instance = get_object_or_404(days, id=id )
-    form = DaysForm(request.POST or None , instance = instance )
-    if form.is_valid():
-        instance = form.save(commit =  False)
-        instance.save()
-    return render(request, 'day.html', {'form':form})
 
 
 @login_required(login_url='login')
@@ -83,6 +86,26 @@ def todays_detail(request):
     return render(request, 'todays_detail.html', {'all_items': all_items})
 
 @login_required(login_url='login')
+def edit_item(request, id=None):
+    instance = get_object_or_404(items, id=id )
+    form = ItemsForm(request.POST or None , instance = instance )
+    if form.is_valid():
+        instance = form.save(commit =  False)
+        instance.save()
+        messages.success(request, "Item's Details are updated!", extra_tags='edit_item')    
+    return render(request, 'show.html', {'form':form})
+
+@login_required(login_url='login')
+def edit_day(request, id=None):
+    instance = get_object_or_404(days, id=id )
+    form = DaysForm(request.POST or None , instance = instance )
+    if form.is_valid():
+        instance = form.save(commit =  False)
+        instance.save()
+        messages.success(request, "Days's Details are updated!", extra_tags='edit_day')
+    return render(request, 'todays_detail.html', {'form':form})
+
+@login_required(login_url='login')
 def delete_item(request, list_id):
     item = items.objects.get(pk=list_id)
     item.delete()
@@ -93,15 +116,7 @@ def delete_item(request, list_id):
 def delete_day(request, list_id):
     day = days.objects.get(pk=list_id)
     day.delete()
-    messages.success(request, 'Day has been deleted!', extra_tags='todays_detail')
+    messages.success(request, "Day's details are deleted!", extra_tags='todays_detail')
     return redirect('todays_detail')
 
  
-"""
-class CreateItemView(CreateView):
-    model = items
-    form_class = ItemsForm
-    template_name = '/home/akbarh/projects/django/srvdy/cal/templates/home.html'
-    success_url = '/home/akbarh/projects/django/srvdy/cal/templates/home.html'
-
-"""
