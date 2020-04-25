dcca2d478a6078033c15a26857445814fae251fc
AASunasara <akbar.h.sunasara@gmail.com>
2020-04-24 19:52:06 -0500

    chnges in databse host

--- srvdy/settings/production.py
+++ srvdy/settings/production.py
@@ -3,3 +3,18 @@ from srvdy.settings.common import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['sarvodaysweets.herokuapp.com',]
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'srvdy',
+        'USER': 'root',
+        'PASSWORD': '8118aass',
+        'HOST': 'sarvodaysweets.herokuapp.com',
+        'PORT':'',
+
+        }
+}
